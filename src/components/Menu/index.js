ce81a1e4c0f1e24d690af0ca03cbd3e2b8d155e1
Joás Lopes <joaslopes6@gmail.com>
2020-08-01 16:38:53 -0300

    Comitão da massa

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Logo from '../../assets/img/Logo.png'
-import './Menu.css'
-import Button from '../Button'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import Button from '../Button';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <Link to="/">
-                <img className="Logo" src={Logo} alt='Classiflix Logo'></img>
-            </Link>
-            
-            <Button as={ Link } className="ButtonLink" to="/cadastro/Video">
-                Novo Vídeo
-            </Button>
-        </nav>
-    )
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="Classiflix Logo" />
+      </Link>
+
+      <Button as={Link} className="ButtonLink" to="/cadastro/Video">
+        Novo Vídeo
+      </Button>
+    </nav>
+  );
 }
 
-export default Menu
+export default Menu;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,18 +1,107 @@
-import React from 'react'
-import PageDefault from '../../../components/PageDefault'
-import { Link } from 'react-router-dom'
+/* eslint-disable linebreak-style */
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-    return (
-        <PageDefault>
-          <h1>Cadastro de Categoria</h1>
+  const valoresIniciais = {
+    nome: '',
+    descrição: '',
+    cor: '',
+  };
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
 
-
-          <Link to="/">
-            Ir para a Home
-          </Link>
-        </PageDefault>
-    )
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
   }
 
-  export default CadastroCategoria
+  function handleChange(infosDoEvento) {
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value,
+    );
+  }
+
+  useEffect(() => {
+    if (window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias';
+      fetch(URL)
+        .then(async (respostaDoServer) => {
+          if (respostaDoServer.ok) {
+            const resposta = await respostaDoServer.json();
+            setCategorias(resposta);
+            return;
+          }
+          throw new Error('Não foi possível pegar os dados');
+        });
+    }
+  }, []);
+
+  return (
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          values,
+        ]);
+
+        setValues(valoresIniciais);
+      }}
+      >
+
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor: "
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.nome}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir para a Home
+      </Link>
+    </PageDefault>
+  );
+}
+
+export default CadastroCategoria;
